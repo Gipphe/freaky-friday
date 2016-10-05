0195295a5e0b3c5b56363a465a091ad1a4f277c0
Victor Nascimento Bakke <gipphe@gmail.com>
2016-10-05 12:37:05 +0200

    Added background color to addToCart and added dud

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-(function(window) {
-	window.addToCart = function() {
-		var cart = document.getElementById('cart-counter');
-		var count = cart.innerHTML;
+(function(global) {
+	'use strict';
+	var timer;
+	global.addToCart = function() {
+		clearTimeout(timer);
+		var counter = document.getElementById('cart-counter');
+		var cart = document.getElementById('cart');
+		var count = counter.innerHTML;
 		count = Number(count);
 		count += 1;
-		cart.innerHTML = count;
+		counter.innerHTML = count;
+		cart.style.backgroundColor = 'white';
+		timer = setTimeout(function() {
+			cart.style.backgroundColor = 'transparent';
+		}, 500);
+	};
+	global.dud = function() {
+		global.alert("Unfortunately, that's not a functioning link, since our task for the Freaky Friday Challenge was only to create ONE page.");
 	};
 }(window));
